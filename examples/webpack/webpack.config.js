b3e5478aeb9df57e120a2c8bcdbaf1dd5b3d4d60
NoamELB <noam@mail.com>
2018-03-07 15:27:36 +0200

    Improving example documentation

--- examples/webpack/webpack.config.js
+++ examples/webpack/webpack.config.js
@@ -4,8 +4,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    // This is quite fast and gives a really good debugging experience (original es6 in sources and when debugging)
-    // Change to 'eval' for faster processing, but es5 in sources
+
+    // 'cheap-module-source-map' is very fast and is a pretty good debugging experience
+    // 'eval' is faster, but sources will be shown as es5
     devtool: 'cheap-module-source-map',
 
     // Defining only 1 application called 'app', and in it, only one entry point -> ./src/index.js
@@ -13,18 +14,17 @@ module.exports = {
         app: ['./src/index.js'],
     },
 
-    // Setting the output in __dirname/bundle.js (path), and make it available in the server on /bundle.js (publicPath)
-    // if using Webpack Dev Server the file will be virtual
+    // Making the bundle available in the server as '/bundle.js'
     output: {
         filename: 'bundle.js',
         publicPath: '/',
         path: path.resolve(__dirname),
     },
+
+    // What are loaders? https://webpack.js.org/concepts/#loaders
     module: {
         rules: [
-            // Babel transpilation
-            // Having the generic env, react and additionally 'react-hot-loader/babel'
-            // for having its functionality in addition to HMR
+            // Babel transpilation - reference: https://github.com/babel/babel-loader
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules.*/,
@@ -32,21 +32,24 @@ module.exports = {
                     {
                         loader: require.resolve('babel-loader'),
                         options: {
+                            // Ignore any .babelrc files, use what is defined below instead
                             babelrc: false,
+                            // Babel preset for ES6+ and React
                             presets: [require.resolve('@babel/preset-env'), require.resolve('@babel/preset-react')],
                             plugins: [
-                                // You need to add this for react-hot-loader to work
+                                // Essential for 'react-hot-loader'
                                 require.resolve('react-hot-loader/babel'),
                             ],
+                            // use node_modules/.cache/babel-loader to cache the results of the loader
                             cacheDirectory: true,
                         },
                     },
                 ],
+                // For a real project you should use Babel with something like https://github.com/amireh/happypack
             },
 
-            // This loader is adding the react-hot-loader's HOC wrapper to every '/App.js' file
-            // Change the regex to put it on any other component
-            // It must be after Babel - to make sure this loader will happen *before* its transpilation
+            // This loader is adding the react-hot-loader's HOC wrapper to every file that match the '/App.js' RegExp
+            // This must appear *after* Babel - to make sure this loader will happen *before* its transpilation
             {
                 test: /\/App\.js$/,
                 loader: require.resolve('react-hot-loader-loader'),
@@ -54,21 +57,23 @@ module.exports = {
 
             // You can add unrelated loaders (non-js) here
             // For example: css pre-processing, images, sprites, etc.
-            // Everything added here will happen to the files before the react-hot-loader-loader loader
+            // Everything added here will happen to the files before react-hot-loader-loader loader
         ],
     },
+
+    // What are plugins? https://webpack.js.org/concepts/#plugins
     plugins: [
-        // This plugin is needed because we don't have any html file, so it will generate a virtual /index.html
+        // This will generate a virtual '/index.html' that includes the '/bundle.js' script
         new HtmlWebpackPlugin(),
 
-        // This plugin is HMR (Hot Module Replacement)
+        // HMR (Hot Module Replacement) activated
         new webpack.HotModuleReplacementPlugin(),
 
-        // This plugin make the logs to be understandable for humans
+        // Make the logs readable for humans (uses file path instead of numbers)
         new webpack.NamedModulesPlugin(),
 
-        // This plugin is super important - it make sure that you will not get a full refresh after errors
-        // that are under App.js (where we are adding the react-hot-loader code)
+        // This is super important - it will make sure that you will not get a full refresh after every error
+        // so react-hot-loader can be used instead to recover after errors
         new webpack.NoEmitOnErrorsPlugin(),
     ],
 };
